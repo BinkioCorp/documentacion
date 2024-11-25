bdf71f263c9058ed5a19439a7b2028b7217097f4
RafaBlockDev <rafafuentesrangel@outlook.com>
2024-11-25 17:41:19 -0600

    feat: init documentation

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './global.css';
+import { RootProvider } from 'fumadocs-ui/provider';
+import { Inter } from 'next/font/google';
+import type { ReactNode } from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>"/>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider>{children}</RootProvider>
+      </body>
+    </html>
+  );
+}
