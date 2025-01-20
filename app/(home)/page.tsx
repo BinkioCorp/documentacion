9c5fe6d35cdd4cc771acd1ba8d7de5c22b12416b
RafaBlockDev <rafafuentesrangel@outlook.com>
2025-01-20 17:22:37 -0600

    feat: upgrade site

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,80 +1,82 @@
 import Image from 'next/image';
+import { ArrowUpRight, Atom } from 'lucide-react';
 import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen mt-10">
+    <div className="min-h-screen p-4 md:p-8">
+      <div className="w-full h-full mx-auto bg-white rounded-[32px] p-6 md:px-8 shadow-lg">
 
-      <section className='flex flex-col items-center justify-center my-10'>
-        <h1 className="text-5xl md:text-8xl font-extrabold text-center mb-6">
-          Make <span className="bg-custom-gradient bg-clip-text text-transparent font-bold">Tokenization</span> real
-        </h1>
-        <p className="text-lg md:text-xl text-center mb-10 max-w-2xl">
-          We make the impossible possible in real estate and crypto. Integrate with Binkio and take your real estate tokenization to the next level: simplicity, transparency, and the power of tokenization all in one place.
-        </p>
-        <Link
-          href="/docs"
-          className="relative text-white font-semibold w-[250px] text-center p-4 rounded border-4 border-transparent bg-black"
-          style={{
-            borderImage: 'linear-gradient(90.2deg, #9A70F1 -0.58%, rgba(80, 60, 147, 0.8) 18.89%, #6A3EF4 37.29%, #D42BBD 74.62%, #F93781 94.63%)',
-            borderImageSlice: 1,
-            backgroundClip: 'padding-box',
-          }}
-        >
-          Get Started
-        </Link>
-      </section>
-
-      <section className='flex flex-row justify-between items-center mt-10 w-full relative flex-wrap md:flex-nowrap'>
-        <div className='w-[80%] relative hidden md:block'>
-          <Image
-            src="https://binkio.s3.us-west-2.amazonaws.com/imagev2.svg"
-            alt="binkio documentation"
-            width={0}
-            height={0}
-            className='w-full h-auto object-contain'
-            style={{
-              transform: 'translateX(-12%)', // Mueve la imagen hacia la izquierda
-              position: 'relative',
-            }}
-          />
-        </div>
-        <div className='flex flex-col items-center justify-center text-center'>
-          <h2 className='text-5xl mb-4'>Modular and <span className='text-[#2053B6]'>Interoperable</span> integration</h2>
-          <p className='mt-4 max-w-[80%]'>Accelerate your market entry with optimized smart contract integrations on our Tokenization-as-a-Service platform</p>
-          <Link href="/docs" className='underline mt-5'>See Documentation</Link>
-        </div>
-      </section>
-
-      <footer className="border-t border-gray-300 w-full md:px-10 p-5 mt-10 flex flex-col lg:flex-row items-center justify-between">
-        <div className="mb-8 lg:mb-0">
-          <Image
-            src="https://binkio.s3.us-west-2.amazonaws.com/Images/Logos/LogoBinkioWhite.svg"
-            alt="Binkio documentation"
-            width={250}
-            height={250}
-            className='mb-4'
-          />
-          <p>Copyright © 2024 Binkio Labs</p>
-        </div>
-
-        <div className='w-full lg:w-auto border border-white flex flex-col lg:flex-row justify-between items-center rounded p-5'>
-          <div className='max-w-full lg:max-w-[50%] mb-4 lg:mb-0'>
-            <h4>Suscribete a nuestro Newsletter</h4>
-            <p>Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet con.</p>
-          </div>
-
-          <div className='flex flex-col lg:flex-row justify-center items-center lg:items-end'>
-            <div className='flex flex-col items-start justify-start mb-4 lg:mb-0'>
-              <label className='text-sm'>Correo electronico</label>
-              <input className='rounded border-2 border-white bg-transparent text-white p-2' />
+        {/* Grid Layout */}
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          {/* Main Offer Card */}
+          <div className="col-span-1 md:col-span-8 bg-zinc-900 rounded-[24px] p-4 md:p-8 relative overflow-hidden">
+            <p className="text-zinc-400 text-sm">Binkio Labs</p>
+            <h1 className="text-white text-6xl font-medium mt-4 mb-8 max-w-md leading-[1.1]">
+                Smart payments tool
+            </h1>
+            <div className="flex items-center gap-2 bg-white rounded-full pl-4 pr-6 py-3 w-fit">
+              <span className="text-zinc-400 text-sm">TECH</span>
+              <div className="flex items-center gap-2 text-zinc-900">
+                <span className="font-medium">Discover</span>
+                <Atom className="h-5 w-5" />
+              </div>
             </div>
-            <button className='bg-[rgba(255,255,255,0.999)] text-black p-3 rounded ml-2 lg:ml-4'><span className='bg-custom-gradient bg-clip-text text-transparent font-bold'>Suscribete</span></button>
+            {/*<Image
+              src=""
+              alt="Product charging case"
+              width={500}
+              height={500}
+              className="absolute bottom-0 right-0 transform translate-y-1/4 translate-x-1/4"
+            />*/}
+          </div>
+
+          {/* Blog Card */}
+          <div className="col-span-1 md:col-span-4 bg-zinc-100 rounded-[24px] p-4 md:p-6 relative overflow-hidden">
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <p className="text-sm text-zinc-900">TECH-DRIVEN PAYMENTS</p>
+                <h2 className="text-3xl font-medium mt-2 md:mt-4">View our blog</h2>
+              </div>
+              <div className="absolute top-4 right-4">
+                <ArrowUpRight className="h-6 w-6 text-zinc-900" />
+              </div>
+            </div>
+            <div className="absolute right-6 top-24">
+              <div className="w-32 h-32 bg-zinc-300 rounded-full" />
+              <div className="w-8 h-8 bg-zinc-900 rounded-full absolute -top-4 right-4" />
+            </div>
+          </div>
+
+          {/* About Us Card */}
+          <div className="col-span-1 md:col-span-6 bg-violet-400 rounded-[24px] p-4 md:p-6 relative">
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <p className="text-white text-sm">DISCOVER</p>
+                <p className="text-white text-sm">OUR HISTORY</p>
+                <h2 className="text-white text-3xl font-medium mt-2">About us</h2>
+              </div>
+              <div className="absolute top-4 right-4">
+                <ArrowUpRight className="h-6 w-6 text-white" />
+              </div>
+            </div>
+          </div>
+
+          {/* Contact Card */}
+          <div className="col-span-1 md:col-span-6 bg-[#B5F459] rounded-[24px] p-4 md:p-6 relative">
+            <div className="flex flex-col justify-between h-full">
+              <div>
+                <p className="text-zinc-900 text-sm">HAVE SOME</p>
+                <p className="text-zinc-900 text-sm">QUESTIONS?</p>
+                <h2 className="text-zinc-900 text-3xl font-medium mt-2 md:mt-4">Contact us</h2>
+              </div>
+              <div className="absolute top-4 right-4">
+                <ArrowUpRight className="h-6 w-6 text-zinc-900" />
+              </div>
+            </div>
           </div>
         </div>
-      </footer>
-
-
-    </main>
+      </div>
+    </div>
   );
 }
